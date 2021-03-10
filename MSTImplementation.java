d7ecefbfb42e18bc037e8d6979c2a69612cbaff8
destinyson7 <tanish.lad@research.iiit.ac.in>
2021-03-10 10:11:34 +0530

    server done

--- MSTImplementation.java
+++ MSTImplementation.java
@@ -1,6 +1,6 @@
 import java.util.*;
 // Implementing the remote interface
-public class ImplExample implements MSTInterface {
+public class MSTImplementation implements MSTInterface {
 
    // Implementing the interface method
    public String printMsg() {
--- MSTInterface.java
+++ MSTInterface.java
@@ -2,8 +2,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 // Creating Remote interface for our application
-public interface MSTInterface extends Remote {
-
+public interface MSTInterface extends Remote
+{
    public void add_graph(String id, int n)
       throws RemoteException;
 
