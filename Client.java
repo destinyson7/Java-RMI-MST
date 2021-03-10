7f611d1a28c0332c55d145609d4610e32852fd44
destinyson7 <tanish.lad@research.iiit.ac.in>
2021-03-10 11:51:00 +0530

    client done

--- Client.java
+++ Client.java
@@ -1,24 +1,67 @@
-import java.rmi.registry.LocateRegistry; 
-import java.rmi.registry.Registry;  
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.*;
 
-public class Client {  
-   private Client() {}  
-   public static void main(String[] args) {  
-      try {  
-         // Getting the registry 
-         Registry registry = LocateRegistry.getRegistry(null); 
-    
-         // Looking up the registry for the remote object 
-         Hello stub = (Hello) registry.lookup("Hello"); 
-    
-         // Calling the remote method using the obtained object 
-         String str = stub.printMsg(); 
-         System.err.println(str);
-         System.out.println("Remote method invoked"); 
-      } catch (Exception e) {
-         System.err.println("Client exception: " + e.toString()); 
-         e.printStackTrace(); 
-      } 
-   } 
+public class Client
+{
+   private Client() {}
+
+   public static void main(String[] args)
+   {
+      try
+      {
+         // Getting the registry
+         String host = args[0];
+         int port = Integer.parseInt(args[1]);
+         Registry registry = LocateRegistry.getRegistry(host, port);
+
+         // Looking up the registry for the remote object
+         MSTInterface stub = (MSTInterface) registry.lookup("MSTInterface");
+
+         // Calling the remote method using the obtained object
+         Scanner scan = new Scanner(System.in);
+
+         while(scan.hasNextLine())
+         {
+            String current = scan.nextLine();
+            String[] arguments = current.split(" ");
+            char request = arguments[0].charAt(4);
+            String id = arguments[1];
+
+            if(request == 'g')
+            {
+               int n = Integer.parseInt(arguments[2]);
+               stub.add_graph(id, n);
+            }
+
+            else if(request == 'e')
+            {
+               int u = Integer.parseInt(arguments[2]);
+               int v = Integer.parseInt(arguments[3]);
+               int w = Integer.parseInt(arguments[4]);
+               stub.add_graph(id, u, v, w);
+            }
+
+            else if(request == 'm')
+            {
+               System.out.println(stub.get_mst(id));
+            }
+
+            else
+            {
+               assert false;
+            }
+         }
+
+         // String str = stub.printMsg();
+         // System.err.println(str);
+         // System.out.println("Remote method invoked");
+      }
+
+      catch (Exception e)
+      {
+         System.err.println("Client exception: " + e.toString());
+         e.printStackTrace();
+      }
+   }
 }
--- MSTImplementation.java
+++ MSTImplementation.java
@@ -2,9 +2,13 @@ import java.util.*;
 // Implementing the remote interface
 public class MSTImplementation implements MSTInterface {
 
-   // Implementing the interface method
-   public String printMsg() {
-      System.out.println("This is an example RMI program");
-      return "Hi";
-   }
+   // Implementing the interface methods
+   public void add_graph(String id, int n)
+      throws RemoteException;
+
+   public void add_edge(String id, int u, int v, int w)
+      throws RemoteException;
+
+   public int get_mst(String id)
+      throws RemoteException;
 }
